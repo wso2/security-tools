ae54fc761be93057fc4c1ae16ce9c48140a2f745
Ayoma Wijethunga <ayoma@wso2.com>
2017-10-11 20:55:34 +0530

    License header

--- product-scanner/src/main/java/org/wso2/security/tools/reposcanner/downloader/DownloadUtil.java
+++ product-scanner/src/main/java/org/wso2/security/tools/reposcanner/downloader/DownloadUtil.java
@@ -1,3 +1,19 @@
+/*
+* Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package org.wso2.security.tools.reposcanner.downloader;
 
 import java.io.File;
@@ -7,9 +23,6 @@ import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 
-/**
- * Created by ayoma on 4/20/17.
- */
 public class DownloadUtil {
     protected static void downloadFile(String sourceUrl, File destinationFile) throws IOException {
         URL website = new URL(sourceUrl);
