034e778cc81ca6f8e7a6270810475e203c51b0bb
prakhash <prakhashsiva21@gmail.com>
2018-02-12 12:34:40 +0530

    adding the samples for handling EntityExpansion

---
+++ samples/TestEntityExpansionLimit/src/main/java/xxe/DemoWithSecurityManager.java
@@ -0,0 +1,74 @@
+/*
+ *   Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *   WSO2 Inc. licenses this file to you under the Apache License,
+ *   Version 2.0 (the "License"); you may not use this file except
+ *   in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package xxe;
+
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.xerces.util.SecurityManager;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+import xxe.entity.Foo;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.sax.SAXSource;
+
+
+/**
+ * DemoWithSecurityManager with overiding the EntityExpansionLimit from xerces.util.SecurityManager
+ */
+
+public class DemoWithSecurityManager {
+    private static final Log log = LogFactory.getLog(DemoWithSecurityManager.class);
+
+    private static void loadAssociationConfig() {
+
+        try {
+            JAXBContext jc = JAXBContext.newInstance(Foo.class);
+            SAXParserFactory spf = SAXParserFactory.newInstance();
+            SAXParser saxParser = spf.newSAXParser();
+            SecurityManager securityManager = new SecurityManager();
+            securityManager.setEntityExpansionLimit(1);
+            saxParser.setProperty("http://apache.org/xml/properties/security-manager", securityManager);
+            XMLReader xmlReader = saxParser.getXMLReader();
+            InputSource inputSource = new InputSource(new FileReader("src/main/resources/input.xml"));
+            SAXSource source = new SAXSource(xmlReader, inputSource);
+            Unmarshaller unmarshaller = jc.createUnmarshaller();
+            Foo foo = (Foo) unmarshaller.unmarshal(source);
+            log.info(foo.getValue());
+        } catch (FileNotFoundException e) {
+            log.error("Failed to find the input.xml", e);
+        } catch (ParserConfigurationException | SAXException | JAXBException e) {
+            log.error("Failed to parse the input.xml", e);
+        }
+    }
+
+    public static void main(String[] args) {
+        loadAssociationConfig();
+    }
+}
---
+++ samples/TestEntityExpansionLimit/src/main/java/xxe/DemoWithoutSecurityManager.java
@@ -0,0 +1,73 @@
+/*
+ *   Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *   WSO2 Inc. licenses this file to you under the Apache License,
+ *   Version 2.0 (the "License"); you may not use this file except
+ *   in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package xxe;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+
+import xxe.entity.Foo;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import javax.xml.XMLConstants;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.sax.SAXSource;
+
+
+
+/**
+ * DemoWithoutSecurityManager without overiding the EntityExpansionLimit from xerces.util.SecurityManager
+ * The default value for EntityExpansion limit from xerces.util.SecurityManager will be used
+ * even an environment property is added for the EntityExpansionLimit.
+ */
+
+public class DemoWithoutSecurityManager {
+    private static final Log log = LogFactory.getLog(DemoWithoutSecurityManager.class);
+
+    private static void loadAssociationConfig() {
+
+        try {
+            JAXBContext jc = JAXBContext.newInstance(Foo.class);
+            SAXParserFactory spf = SAXParserFactory.newInstance();
+            spf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+            XMLReader xmlReader = spf.newSAXParser().getXMLReader();
+            InputSource inputSource = new InputSource(new FileReader("src/main/resources/input.xml"));
+            SAXSource source = new SAXSource(xmlReader, inputSource);
+            Unmarshaller unmarshaller = jc.createUnmarshaller();
+            Foo foo = (Foo) unmarshaller.unmarshal(source);
+            log.info(foo.getValue());
+        } catch (FileNotFoundException e) {
+            log.error("Failed to find the input.xml", e);
+        } catch (ParserConfigurationException | SAXException | JAXBException e) {
+            log.error("Failed to parse the input.xml", e);
+        }
+    }
+
+    public static void main(String[] args) {
+        loadAssociationConfig();
+    }
+
+}
