b4c3f9d7e2922b97112eaecfdb6616af23003c24
kasundharmadasa <kasunpdh@gmail.com>
2018-10-05 18:12:55 +0530

    improving error messages in find bugs scanner agent

--- internal/automation-manager/agent/find-sec-bugs-scanner/src/main/java/org/wso2/security/tools/findsecbugs/scanner/NotificationManager.java
+++ internal/automation-manager/agent/find-sec-bugs-scanner/src/main/java/org/wso2/security/tools/findsecbugs/scanner/NotificationManager.java
@@ -1,20 +1,20 @@
 /*
-*  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-*  WSO2 Inc. licenses this file to you under the Apache License,
-*  Version 2.0 (the "License"); you may not use this file except
-*  in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
+ *  Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
 package org.wso2.security.tools.findsecbugs.scanner;
 
@@ -37,12 +37,12 @@ import java.net.URISyntaxException;
  */
 public class NotificationManager {
 
-    private final static String NOTIFY = "automationManager/staticScanner/notify";
-    private final static String FILE_UPLOADED = NOTIFY + "/fileUploaded";
-    private final static String FILE_EXTRACTED = NOTIFY + "/fileExtracted";
-    private final static String PRODUCT_CLONED = NOTIFY + "/productCloned";
-    private final static String SCAN_STATUS = NOTIFY + "/scanStatus";
-    private final static String REPORT_READY = NOTIFY + "/reportReady";
+    private static final String NOTIFY = "automationManager/staticScanner/notify";
+    private static final String FILE_UPLOADED = NOTIFY + "/fileUploaded";
+    private static final String FILE_EXTRACTED = NOTIFY + "/fileExtracted";
+    private static final String PRODUCT_CLONED = NOTIFY + "/productCloned";
+    private static final String SCAN_STATUS = NOTIFY + "/scanStatus";
+    private static final String REPORT_READY = NOTIFY + "/reportReady";
     private static final Logger LOGGER = LoggerFactory.getLogger(NotificationManager.class);
     private static String myContainerId;
     private static String automationManagerHost;
@@ -84,11 +84,11 @@ public class NotificationManager {
                 }
                 Thread.sleep(2000);
             } catch (URISyntaxException | InterruptedException | IOException e) {
-                e.printStackTrace();
-                LOGGER.error(e.toString());
+                LOGGER.error("Error occurred while notifying the status to automation manager", e);
                 i++;
             }
-        }throw new NotificationManagerException("Error occurred while notifying status to Automation Manager");
+        }
+        throw new NotificationManagerException("Error occurred while notifying status to Automation Manager");
     }
 
     public static void notifyScanStatus(String status) throws NotificationManagerException {
@@ -111,11 +111,11 @@ public class NotificationManager {
                 }
                 Thread.sleep(2000);
             } catch (URISyntaxException | InterruptedException | IOException e) {
-                e.printStackTrace();
-                LOGGER.error(e.toString());
+                LOGGER.error("Error occurred while notifying the scan status to automation manager", e);
                 i++;
             }
-        }throw new NotificationManagerException("Error occurred while notifying status to Automation Manager");
+        }
+        throw new NotificationManagerException("Error occurred while notifying status to Automation Manager");
     }
 
     /**
@@ -160,6 +160,7 @@ public class NotificationManager {
 
     /**
      * Returns if the {@code NotificationManager} configured
+     *
      * @return Boolean value
      */
     public static boolean isConfigured() {
