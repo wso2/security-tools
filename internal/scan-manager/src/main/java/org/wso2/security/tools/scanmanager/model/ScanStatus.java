e64e7334c34822b56fd0e1f4b11aba77ac0194a2
Sachin Ramesh <38999745+sacheeramesh@users.noreply.github.com>
2019-04-01 10:52:37 +0530

    Update ScanStatus.java

--- internal/scan-manager/src/main/java/org/wso2/security/tools/scanmanager/model/ScanStatus.java
+++ internal/scan-manager/src/main/java/org/wso2/security/tools/scanmanager/model/ScanStatus.java
@@ -20,5 +20,5 @@ package org.wso2.security.tools.scanmanager.model;
  * Model Enum to represent a Status of the Scans.
  */
 public enum ScanStatus {
-    PRNDING, SUBMITTED, RUNNING, CANCELED, COMPLETED, FINISHED, ERROR, FAILED
+    PNDING, SUBMITTED, RUNNING, CANCELED, COMPLETED, FINISHED, ERROR, FAILED
 }
