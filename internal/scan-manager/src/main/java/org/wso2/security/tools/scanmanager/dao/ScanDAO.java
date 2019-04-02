3a07b5e9943d0137e1fffe5bcd886c3b2175cdb7
Sachin Ramesh <38999745+sacheeramesh@users.noreply.github.com>
2019-04-02 08:49:18 +0530

    Update ScanDAO.java

--- internal/scan-manager/src/main/java/org/wso2/security/tools/scanmanager/dao/ScanDAO.java
+++ internal/scan-manager/src/main/java/org/wso2/security/tools/scanmanager/dao/ScanDAO.java
@@ -5,6 +5,7 @@ import org.wso2.security.tools.scanmanager.model.Scan;
  * The class {@code } is the DAO class that persist data on the SCAN table.
  */
 public interface ScanDAO {
+    
     boolean persist(Scan scan);
     
     Scan getScan(Integer scanId);
