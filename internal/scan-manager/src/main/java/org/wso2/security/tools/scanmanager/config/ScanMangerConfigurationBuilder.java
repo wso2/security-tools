bb2466a31a3d4ef58b57b1def0d3c5d34acd6426
Sachin Ramesh <38999745+sacheeramesh@users.noreply.github.com>
2019-04-02 08:35:47 +0530

    Update ScanMangerConfigurationBuilder.java

--- internal/scan-manager/src/main/java/org/wso2/security/tools/scanmanager/config/ScanMangerConfigurationBuilder.java
+++ internal/scan-manager/src/main/java/org/wso2/security/tools/scanmanager/config/ScanMangerConfigurationBuilder.java
@@ -18,7 +18,6 @@
 
 package org.wso2.security.tools.scanmanager.config;
 
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.wso2.security.tools.scanmanager.exception.ScanManagerException;
