88672531a1a0ad4e604a6271f50fa6e0072426d5
Sachin Ramesh <38999745+sacheeramesh@users.noreply.github.com>
2019-04-02 13:15:55 +0530

    Update LogServiceImpl.java

--- internal/scan-manager/src/main/java/org/wso2/security/tools/scanmanager/service/LogServiceImpl.java
+++ internal/scan-manager/src/main/java/org/wso2/security/tools/scanmanager/service/LogServiceImpl.java
@@ -26,7 +26,6 @@ import org.wso2.security.tools.scanmanager.model.Log;
 /**
  * The class {@code LogService} is the service class that manage the method implementations of the Scan logs.
  */
-
 @Service
 public class LogServiceImpl implements LogService {
 
