8ff87d0710b0d7039256d4948bab9753f501adf2
NShani <pnadeeshani65@gmail.com>
2019-06-24 14:17:26 +0530

    Update internal/scan-manager/scanners/common/src/main/java/org/wso2/security/tools/scanmanager/scanners/common/ScannerController.java
    Co-Authored-By: Ayoma Wijethunga <ayomawdb@gmail.com>

--- internal/scan-manager/scanners/common/src/main/java/org/wso2/security/tools/scanmanager/scanners/common/ScannerController.java
+++ internal/scan-manager/scanners/common/src/main/java/org/wso2/security/tools/scanmanager/scanners/common/ScannerController.java
@@ -153,7 +153,6 @@ public class ScannerController {
     }
 
     private boolean stopStartScanThread() {
-
         while (startScanThread.isAlive()) {
             // run until the start scan thread is dead.
         }
