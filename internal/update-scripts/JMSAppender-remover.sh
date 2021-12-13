6d173720a9b8ff8a78999a767e809e204421eb14
Ayoma Wijethunga <ayomawdb@gmail.com>
2021-12-13 23:12:23 +0530

    Update JMSAppender-remover.sh

--- internal/update-scripts/JMSAppender-remover.sh
+++ internal/update-scripts/JMSAppender-remover.sh
@@ -6,8 +6,8 @@ JAR_FILES=$(find . -name '*.jar')
 for JAR_FILE in $JAR_FILES
 do
     echo "[-] Checking: $JAR_FILE" >> "log4j_jms_remover_$TIMESPTAMP.log"
-    CONTAINS_JNDI_LOOKUP=$(unzip -l "$JAR_FILE" | grep "org/apache/log4j/net/JMSAppender.class")
-    if [ -n "$CONTAINS_JNDI_LOOKUP" ]; then
+    CONTAINS_TARGET=$(unzip -l "$JAR_FILE" | grep "org/apache/log4j/net/JMSAppender.class")
+    if [ -n "$CONTAINS_TARGET" ]; then
         echo "Found org/apache/log4j/net/JMSAppender in: $JAR_FILE" >> "log4j_jms_remover_$TIMESPTAMP.log"
         echo "Removing org/apache/log4j/net/JMSAppender from $JAR_FILE" | tee -a "log4j_jms_remover_$TIMESPTAMP.log"
         zip -d "$JAR_FILE" org/apache/log4j/net/JMSAppender.class >>  "log4j_jms_remover_$TIMESPTAMP.log" 2>&1
