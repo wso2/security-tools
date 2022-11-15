11df48ad35a55b60e16fa17b92e45c5cd08c7f52
priyan0001 <ppritasuthan@gmail.com>
2022-11-15 23:47:17 +0530

    new commit

--- internal/Intern Automation tools/Automation code.py
+++ internal/Intern Automation tools/Automation code.py
@@ -3,38 +3,83 @@ import whois
 import nmap
 import sublist3r
 import dns.resolver
+import datetime 
+import emailprotectionslib.dmarc as dmarclib
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
+SCOPES = [
+    'https://www.googleapis.com/auth/drive',
+    'https://www.googleapis.com/auth/drive.file'
+    ]
+SPREADSHEET_ID = '1_Q2_Bjnm8ERDYcggsMVCPPWCDGB-s4fCUDejJvIqnc8'
+GET_IP_RANGE = 'results!A2:A160'
+GET_DATE_RANGE = 'date!A1'
+
+scanDateTime = datetime.datetime.now()
+SCAN_DATE = scanDateTime.strftime("%x")
+
+# Retrieves data from Google Sheet with any given range
+def getData(service, range):
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID,
+                                range=range,
+                                valueRenderOption='FORMATTED_VALUE').execute()
+    values = result.get('values', [])
+
+    if not values:
+        print('Cannot retreive data. No data found.')
+        quit()
+
+    return values
+
+#subdoamin eumeration
 print("----------------------------------------------------")
 print("Subdomain Enumeration")
 domain=input("Enter the Domain name : ")
 subdomains = sublist3r.main(domain, 40, 'subdomains.txt', ports= None, silent=False, verbose= False, enable_bruteforce= False, engines=None)
 
-print("----------------------------------------------------")
-print("IP address")
-d = open("subdomains.txt",'r').read().splitlines()
-for domain in d:
-    try:
-        result = dns.resolver.resolve(domain, 'A')
-        for val in result:
-            print(domain ,' : ', val.to_text())
-            f = open("IP.txt", "a")
-            f.write(val.to_text())
-            f.write('\n')
-            f.close()
-          
-    except:
-        print(domain, " : can't resolve")
-        f = open("IP.txt", "a")
-        f.write("can't resolve") 
-        f.write('\n')
-        f.close()
+#DNS records
+record_types = ['A', 'AAAA', 'NS', 'CNAME', 'MX', 'PTR', 'SOA', 'TXT']
+try:
+    domain =open(r"C:\Users\WSO2\Desktop\security-tools\internal\Intern Automation tools\subdomain.txt").read().splitlines()
+except IndexError:
+    print('Syntax error - python3 dnsenum.py domainname')
 
+for url in domain:
+    for records in record_types:
+        try:
+            answer = dns.resolver.resolve(url, records)
+            print(f'\n{records} Records')
+            print('-' * 30)
+            for server in answer:
+                print(str(server))
+        except dns.resolver.NoAnswer:
+            pass
+        except dns.resolver.NXDOMAIN:
+            print(f'{url} does not exist.')
+            quit()
+        except KeyboardInterrupt:
+            print('Quitting.')
+            quit()
+
+dmarc=dmarclib.DmarcRecord.from_domain(url)
+print("\n DMARC Records")
+print('-' * 30)
+print(dmarc , '\n')
+
+#Whois scan 
 print("----------------------------------------------------")
 print("WHOIS scan")
 domain_info= whois.whois(domain)
 for key, value in domain_info.items():
     print(key,':',value)
 
+
+#Port scan
 print("----------------------------------------------------")
 scanner = nmap.PortScanner()
 print("NMAP automation scan")
--- internal/Intern Automation tools/DNS check/DNScheck.py
+++ internal/Intern Automation tools/DNS check/DNScheck.py
@@ -1,26 +1,150 @@
-import dns.resolver
-import sys
 import emailprotectionslib.dmarc as dmarclib
-record_types = ['A', 'AAAA', 'NS', 'CNAME', 'MX', 'PTR', 'SOA', 'TXT']
-try:
-    domain =open(r"C:\Users\WSO2\Desktop\Outputs\Subdomain.txt").read().splitlines()
-except IndexError:
-    print('Syntax error - python3 dnsenum.py domainname')
+import csv
+import dns.resolver
+import pandas as pd
+import gspread
+import os
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
 
-for url in domain:
-    for records in record_types:
-        try:
-            answer = dns.resolver.resolve(url, records)
-            print(f'\n{records} Records')
-            print('-' * 30)
-            for server in answer:
-                print(server.to_text())
-                
-        except dns.resolver.NXDOMAIN:
-            print(f'{url} does not exist.')
-            quit()
+def Arecord(url):
+    try:
+        ARec = dns.resolver.resolve(url,'A')
+        for server1 in ARec:
+            return server1
+    except dns.resolver.NoAnswer:
+        pass
+    except dns.resolver.NXDOMAIN:
+        print(f'{url} does not exist.')
+        quit()
+    except KeyboardInterrupt:
+        return('Quitting.')
+        quit()
 
-dmarc=dmarclib.DmarcRecord.from_domain(url)
-print("\n DMARC Records")
-print('-' * 30)
-print(dmarc , '\n')
+def AAAArecord(url):
+    try:
+        AAAARec = dns.resolver.resolve(url,'AAAA')
+        for server2 in AAAARec:
+            print(str(server2))
+            return server2
+    except dns.resolver.NoAnswer:
+        pass
+    except dns.resolver.NXDOMAIN:
+        print(f'{url} does not exist.')
+        quit()
+    except KeyboardInterrupt:
+        return('Quitting.')
+        quit()
+    
+def NSrecord(url):
+    try:
+        answer3 = dns.resolver.resolve(url,'NS')
+        for server3 in answer3:
+            print(str(server3))
+            return server3
+    except dns.resolver.NoAnswer:
+        pass
+    except dns.resolver.NXDOMAIN:
+        print(f'{url} does not exist.')
+        quit()
+    except KeyboardInterrupt:
+        return('Quitting.')
+        quit()
+
+def CNAME(url):
+    try:
+        answer4 = dns.resolver.resolve(url,'CNAME')
+        for server4 in answer4:
+            return server4
+    except dns.resolver.NoAnswer:
+        pass
+    except dns.resolver.NXDOMAIN:
+        print(f'{url} does not exist.')
+        quit()
+    except KeyboardInterrupt:
+        return('Quitting.')
+        quit()
+
+def MXRecord(url):
+    try:
+        answer5 = dns.resolver.resolve(url,'TXT')
+        for server5 in answer5:
+            print(str(server5))
+            return server5
+    except dns.resolver.NoAnswer:
+        pass
+    except dns.resolver.NXDOMAIN:
+        print(f'{url} does not exist.')
+        quit()
+    except KeyboardInterrupt:
+        return('Quitting.')
+        quit()
+
+
+def dmarcRecord(url: str):
+    try:
+        dmarc=dmarclib.DmarcRecord.from_domain(url)
+        return dmarc
+    except KeyboardInterrupt:
+        print('Quitting.')
+        quit()
+  
+url_list = []
+A_list = []
+AAAA_list=[]
+NS_list=[]
+CNAME_list=[]
+MXRecord_list=[]
+#dmarc_list=[]
+
+def main():
+    SCOPES = [
+        'https://www.googleapis.com/auth/drive',
+        'https://www.googleapis.com/auth/drive.file'
+        ]
+    creds = None
+
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    gc = gspread.authorize(creds)
+
+    # Read CSV file contents
+    content = open(r'file.csv').read()
+
+    gc.import_csv('<1f2voEeVmq_McHAfD_Y08rIJGcY44EbNkLN2Ni8io6zI>', content)
+
+
+csvfile= open('file.csv', 'w')
+list = open(r"C:\Users\WSO2\Desktop\csv\Subdomain.txt").read().splitlines()
+for url in list:
+    print ((url), Arecord(url), AAAArecord(url),NSrecord(url),CNAME(url),MXRecord(url))
+    data = [url,Arecord(url),AAAArecord(url),NSrecord(url),CNAME(url),MXRecord(url)]
+    #data_list.append(data)
+    url_list.append(url)
+    A_list.append(Arecord(url))
+    AAAA_list.append(AAAArecord(url))
+    NS_list.append(NSrecord(url))
+    CNAME_list.append(CNAME(url))
+    MXRecord_list.append(MXRecord(url))
+    #dmarc_list.append(dmarcRecord(url))
+
+#print data_list
+writer = csv.writer(csvfile, dialect='excel')
+writer.writerows(zip(url_list, A_list, AAAA_list, NS_list, CNAME_list, MXRecord_list))
+
+if __name__ == '__main__':
+    main()
+
